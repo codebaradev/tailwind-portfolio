069954d1dd5ed06cb337e48350d499ff80b92bce
codebara <bahnasep@gmail.com>
2025-03-05 22:50:44 +0800

    feat: Hero Section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        default: "17px",
+        sm: "1rem",
+        md: "5rem"
+      }
+    },
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        dark: "#0f172a"
+      },
+      screens: {
+        'xl' : '1250px'
+      }
+    },
   },
   plugins: [],
 }
